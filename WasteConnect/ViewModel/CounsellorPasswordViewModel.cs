8a275a70aa3ecba60c69655669c707e74cfac25c
Refilwe Nkosi <refilwe.nkosi2277@gmail.com>
2026-07-20 13:47:05 +0200

    Created Counsellor ViewModel for password and creating Counsellor

---
+++ WasteConnect/ViewModel/CounsellorPasswordViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WasteConnect.ViewModel
+{
+    public class CounsellorPasswordViewModel
+    {
+
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+
+        [Required]
+        public string Token { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 8)]
+        [Display(Name = "New Password")]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+    }
+}
--- WasteConnect/ViewModel/CounsellorViewModel.cs
+++ WasteConnect/ViewModel/CounsellorViewModel.cs
@@ -1,6 +1,32 @@
-﻿namespace WasteConnect.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WasteConnect.ViewModel
 {
     public class CounsellorViewModel
     {
+
+        [Required]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [Phone]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Ward Number")]
+        [Range(1, 100)]
+        public int WardNumber { get; set; }
+
+        [Display(Name = "Position")]
+        public string PositionTitle { get; set; } = "Ward Counsellor";
+
+
     }
 }
