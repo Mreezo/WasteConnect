46cdbc41788ae0382612965bb73eb8a03c26f15c
Refilwe Nkosi <refilwe.nkosi2277@gmail.com>
2026-07-31 00:35:59 +0200

    Added councillor dashboard view model

---
+++ WasteConnect/ViewModel/CouncillorDashboardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using WasteConnect.Models;
+
+namespace WasteConnect.ViewModels
+{
+    public class CouncillorDashboardViewModel
+    {
+        public string CouncillorName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PositionTitle { get; set; } = "Ward Councillor";
+
+        public int WardNumber { get; set; }
+
+        public List<DumpingReport> Reports { get; set; } = new();
+    }
+}
